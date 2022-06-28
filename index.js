b93ce21bec67dd11336cb966dd654cfd95d6fc73
ivan-martinm <ivan87.martin@gmail.com>
2022-06-28 13:34:41 +0200

    added phonebook-backend step7

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny')) // Using tiny predefined format
+// Below is the equivalent using predefined tokens
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 let persons = [
     {
