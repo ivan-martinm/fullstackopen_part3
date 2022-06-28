8f3cd0d47d1e1484e8ba776bac55f8abfa65e06d
ivan-martinm <ivan87.martin@gmail.com>
2022-06-28 11:41:48 +0200

    added phonebook-backend step2

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ let persons = [
         "id": 1,
         "name": "Arto Hellas",
         "number": "040-123456"
-    },{
+    }, {
         "id": 2,
         "name": "Ada Lovelace",
         "number": "39-44-5323523"
-    },{
+    }, {
         "id": 3,
         "name": "Dan Abramov",
         "number": "12-43-234345"
-    },{
+    }, {
         "id": 4,
         "name": "Mary Poppendieck",
         "number": "39-23-6423122"
@@ -23,6 +23,13 @@ let persons = [
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${Date()}</p>`
+    )
 })
 
 const PORT = 3001
